a6fecf0f510647a930c7f52d5b11e792522ad02c
itmaybehimm <himansupradhan472@gmail.com>
2024-02-27 20:55:16 +0545

    sdasd

--- Backend/BYDUsers/BYDUsers/settings.py
+++ Backend/BYDUsers/BYDUsers/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-from decouple import config, Csv
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
